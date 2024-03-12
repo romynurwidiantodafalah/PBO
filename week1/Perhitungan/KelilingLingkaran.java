ecb42b1f98dbc77dd622d76479f828d35b0806dd
Romy <110410235+romynurwidiantodafalah@users.noreply.github.com>
2024-03-12 15:39:49 +0700

    Update KelilingLingkaran.java

--- week1/Perhitungan/KelilingLingkaran.java
+++ week1/Perhitungan/KelilingLingkaran.java
@@ -5,10 +5,10 @@ public class KelilingLingkaran {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double diameter = 10;
-        double phi = 3.14;
-        double keliling = phi * diameter;
+        	double phi = 3.14;
+        	double keliling = phi * diameter;
         
-        System.out.println("Keliling lingkaran dengan diameter " + diameter + " adalah " + keliling);
+        	System.out.println("Keliling lingkaran dengan diameter " + diameter + " adalah " + keliling);
 	}
 
 }
