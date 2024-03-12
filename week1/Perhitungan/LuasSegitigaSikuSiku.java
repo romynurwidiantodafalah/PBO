93fc6675b2f4a68713b3ff335cef734ccb2adb5b
Romy <110410235+romynurwidiantodafalah@users.noreply.github.com>
2024-03-12 15:40:13 +0700

    Update LuasSegitigaSikuSiku.java

--- week1/Perhitungan/LuasSegitigaSikuSiku.java
+++ week1/Perhitungan/LuasSegitigaSikuSiku.java
@@ -5,10 +5,10 @@ public class LuasSegitigaSikuSiku {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double alas = 6;
-        double tinggi = 8;
-        double luas = (0.5 * alas * tinggi);
+        	double tinggi = 8;
+        	double luas = (0.5 * alas * tinggi);
 
-        System.out.println("Luas segitiga siku-siku dengan alas " + alas + " dan tinggi " + tinggi + " adalah " + luas);
+        	System.out.println("Luas segitiga siku-siku dengan alas " + alas + " dan tinggi " + tinggi + " adalah " + luas);
 	}
 
 }
