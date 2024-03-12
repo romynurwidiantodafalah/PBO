44d9aafb72c9aa670df695a7c8b693e73dd7c4ed
Romy <110410235+romynurwidiantodafalah@users.noreply.github.com>
2024-03-12 15:40:32 +0700

    Update VolumeTabung.java

--- week1/Perhitungan/VolumeTabung.java
+++ week1/Perhitungan/VolumeTabung.java
@@ -5,12 +5,12 @@ public class VolumeTabung {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double diameter = 5;
-        double tinggi = 10;
-        double phi = 3.14;
+        	double tinggi = 10;
+        	double phi = 3.14;
 
-        double volume = (0.25 * phi * diameter * diameter * tinggi); //0.25 atau 1/4
+        	double volume = (0.25 * phi * diameter * diameter * tinggi); //0.25 atau 1/4
 
-        System.out.println("Volume tabung dengan diameter " + diameter + " dan tinggi " + tinggi + " adalah " + volume);
+        	System.out.println("Volume tabung dengan diameter " + diameter + " dan tinggi " + tinggi + " adalah " + volume);
 	}
 
 }
