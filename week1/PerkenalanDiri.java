e48f1a3f0264e9eca5fd23f0acf8a2e08cc24108
Romy <110410235+romynurwidiantodafalah@users.noreply.github.com>
2024-03-12 15:39:17 +0700

    Update PerkenalanDiri.java

--- week1/PerkenalanDiri.java
+++ week1/PerkenalanDiri.java
@@ -5,8 +5,8 @@ public class PerkenalanDiri {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Halo, nama saya adalah Romy Nur Widianto Dafalah");
-        System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
-        System.out.println("Saya sedang menulis program Java");
+        	System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+        	System.out.println("Saya sedang menulis program Java");
 	}
 
 }
